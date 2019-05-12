66c97e61e5ec6d1ab833c6f4f8139835e1cca282
lanche58 <675603402@qq.com>
2019-05-12 23:55:00 +0800

    第一次上传

--- src/js/copy/page.js
+++ src/js/copy/page.js
@@ -91,51 +91,55 @@ $(function () {
     // });
 
 
-
+    if ($('.splwo').length !== 0) {
+        $('.splwo').each(function(i) {
+            splitWords($(this));
+        });
+    }
 
 	// 手机导航
-	// $('.navMobile dd > a').bind(_click, function (e) {
-	// 	if($(this).next('.mtv').size() >= 1){
-	// 		if(!$(this).hasClass('act')){
-	// 			e.preventDefault();
-	// 			$('.navMobile dd > a').removeClass('act');
-	// 			$('.mtv').stop().slideUp(300);
-	// 			$(this).addClass('act');
-	// 			$(this).next('.mtv').stop().slideDown(300);
-	// 		}else{
-	// 			$(this).removeClass('act');
-	// 			$(this).next('.mtv').stop().slideUp(300);
-	// 		}
-	// 	}
-	// });
-	// $menuBtn.bind(_click, function () {
- //        var w = $menuBox.width();
- //        if (navItem == 0) {
- //            $('html').addClass('open');
- //            $('.menuBlack').stop().fadeIn(600);
- //            $(this).addClass('active');
- //            $menuBox.show().stop(false,false).animate({right:0});
- //            navItem = 1;
- //        } else {
- //            $('html').removeClass('open');
- //            $('.menuBlack').stop().fadeOut(600);
- //            $(this).removeClass('active').stop(false,false).animate({right:0});
- //            $menuBox.stop(false,false).animate({right:-w+"px"},function(){
- //                $(this).hide();
- //            });
- //            navItem = 0;
- //        };
- //    });
- //    $('.menuBlack').bind(_click, function () {
- //        var w = $menuBox.width();
- //        $('html').removeClass('open');
- //        $menuBtn.removeClass('active');
- //        $('.menuBlack').stop().fadeOut(600);
- //        $menuBox.stop(false,false).animate({right:-w+"px"},function(){
- //            $(this).hide();
- //        });
- //        navItem = 0;
- //    });
+	$('.navMobile dd > a').bind(_click, function (e) {
+		if($(this).next('.mtv').size() >= 1){
+			if(!$(this).hasClass('act')){
+				e.preventDefault();
+				$('.navMobile dd > a').removeClass('act');
+				$('.mtv').stop().slideUp(300);
+				$(this).addClass('act');
+				$(this).next('.mtv').stop().slideDown(300);
+			}else{
+				$(this).removeClass('act');
+				$(this).next('.mtv').stop().slideUp(300);
+			}
+		}
+	});
+	$menuBtn.bind(_click, function () {
+        var w = $menuBox.width();
+        if (navItem == 0) {
+            $('html').addClass('open');
+            $('.menuBlack').stop().fadeIn(600);
+            $(this).addClass('active');
+            $menuBox.show().stop(false,false).animate({right:0});
+            navItem = 1;
+        } else {
+            $('html').removeClass('open');
+            $('.menuBlack').stop().fadeOut(600);
+            $(this).removeClass('active');
+            $menuBox.stop(false,false).animate({right:-w+"px"},function(){
+                $(this).hide();
+            });
+            navItem = 0;
+        };
+    });
+    $('.menuBlack').bind(_click, function () {
+        var w = $menuBox.width();
+        $('html').removeClass('open');
+        $menuBtn.removeClass('active');
+        $('.menuBlack').stop().fadeOut(600);
+        $menuBox.stop(false,false).animate({right:-w+"px"},function(){
+            $(this).hide();
+        });
+        navItem = 0;
+    });
 	
 	
 	// pbanner animation
@@ -299,10 +303,13 @@ function splitWords(el) {
         for (var j = 0; j < _test[i].length; j++) {
             _span.eq(i).append('<i></i>');
             var _i = _span.eq(i).find('i');
-            _i.eq(j).text(_test[i][j]).css({
+            if (_test[i][j] === " ") {
+                _test[i][j] = "&nbsp;";
+            }
+            _i.eq(j).html(_test[i][j]).css({
                 'animation-delay': 100*j + 'ms',
                 '-webkit-animation-delay': 100*j + 'ms'
             });
         }
     }
-};
+}
